6ee3ed397464a1b946a5b34110c20af93240e713
Saimon Shaplygin <semyuon@gmail.com>
2024-11-17 15:33:43 +0100

    add: bottom menu and toolbar

--- app/src/main/java/com/example/morsecode/MainActivity.kt
+++ app/src/main/java/com/example/morsecode/MainActivity.kt
@@ -3,21 +3,21 @@ package com.example.morsecode
 import android.content.Context
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.morsecode.ui.theme.MorseCodeTheme
-import org.w3c.dom.Text
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.morsecode.menu.EducationFragment
+import com.example.morsecode.menu.HomeFragment
+import com.example.morsecode.menu.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : ComponentActivity() {
 
     private var isFlashlightOn = false
+    private lateinit var drawerLayout: DrawerLayout
 
     private fun toggleFlashlight(context: Context) {
         val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager;
@@ -39,39 +39,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.textView)
-        textView.text = "Hello there!"
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        AppCompatActivity().setSupportActionBar(toolbar)
 
-        val button : Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            toggleFlashlight(applicationContext)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> loadFragment(HomeFragment())
+                R.id.nav_education ->  loadFragment(EducationFragment())
+                R.id.nav_settings ->loadFragment(SettingsFragment())
+            }
+            true
         }
-//        enableEdgeToEdge()
-//        setContent {
-//            MorseCodeTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
-//        }
+
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment())
+        }
     }
+
+    private fun loadFragment(fragment: Fragment) {
+        AppCompatActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
+//    override fun onBackPressed() {
+//        val view: View = findViewById(R.id.bottom_navigation)
+//        if (drawerLayout.isDrawerOpen(view)) {
+//            drawerLayout.closeDrawers()
+//        } else {
+//            super.onBackPressed()
+//        }
+//    }/
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    MorseCodeTheme {
-//        Greeting("Android")
-//    }
-//}
